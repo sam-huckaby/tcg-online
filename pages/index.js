b1d52052a397fc79ed451c66ff29b999f3820f18
Sam Huckaby <samhuckaby@gmail.com>
2021-12-10 13:51:17 -0700

    Simple page updates for index just to see Vercel in action.

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Trading Card Games - Coach
         </h1>
 
         <p className={styles.description}>
@@ -23,30 +23,30 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Card Dictionary &rarr;</h2>
+            <p>Find in-depth information about various TCG cards.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>Learn about TCGs and how to play them from experts.</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Play Groups &rarr;</h2>
+            <p>Search for people playing your TCG near you.</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Purchase &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Buy new cards to fuel your collection and love of the game.
             </p>
           </a>
         </div>
