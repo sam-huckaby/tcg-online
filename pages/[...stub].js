e8e0fa2effaf0ceeaebd5d1d07572f7896037836
Sam Huckaby <samhuckaby@gmail.com>
2021-12-14 16:58:28 -0700

    Convert CSS to SCSS and implement the 404 page

--- pages/[...stub].js
+++ pages/[...stub].js
@@ -1,12 +1,16 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from '/styles/404.module.scss';
 
 const Lost = () => {
     const router = useRouter();
     const { stub } = router.query;
-    console.log(stub);
-    let missed = stub.join('/');
+    let missed = '';
+    
+    if (stub) {
+        missed = stub.join('/');
+    }
 
     return (
         <div className={styles.container}>
@@ -17,13 +21,20 @@ const Lost = () => {
             </Head>
             <main className={styles.main}>
                 <div className={styles.tiltSection}>
+                    <Link href="/">
+                        <a>
+                            <h1 className={styles['big-title']}>
+                                <span id={styles.t_card}>T</span><span id={styles.c_card}>C</span><span id={styles.g_card}>G</span> Online
+                            </h1>
+                        </a>
+                    </Link>
                     <h1 className={styles.title}>
-                        We exhausted our library, but couldn't find:
+                        We exhausted our library, but couldn't find that one.
                     </h1>
 
-                    <p className={styles.description}>
+                    {/* <p className={styles.description}>
                         `/{missed}`
-                    </p>
+                    </p> */}
                 </div>
             </main>
         </div>
