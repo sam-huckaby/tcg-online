325bf1d1a60eca5df761219986741417888e57ec
Sam Huckaby <samhuckaby@gmail.com>
2022-01-03 10:41:37 -0700

    [FEATURE] Leverage Sanity.io for content creation and update headers and footers

---
+++ components/common/miniHeader.component.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+
+import styles from '../../styles/common/miniHeader.module.scss';
+
+
+class MiniHeader extends Component {
+    // This constructor may be unnecessary
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={styles['header-container']}>
+                <div className={styles.tiltSection}>
+                    <Link href="/">
+                        <div className={styles.title}>
+                            <div><span id={styles.t_card}>T</span><span id={styles.c_card}>C</span><span id={styles.g_card}>G</span></div>
+                            <div className={styles['online-subtext']}>Online</div>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+            
+        );
+    }
+}
+
+export default MiniHeader;
---
+++ pages/learn/mtg/[slug].js
@@ -0,0 +1,78 @@
+import groq from 'groq';
+import BlockContent from '@sanity/block-content-to-react'
+import Head from 'next/head';
+
+import client from '../../../client';
+
+import MiniHeader from '../../../components/common/miniHeader.component';
+import ArticleFooter from '../../../components/common/miniFooter.component';
+
+import styles from '../../../styles/learn/mtg/post.module.scss';
+
+const Post = (props) => {
+    const { title = 'Missing title', name = 'Unknown', categories, body = [] } = props.post
+    
+    return (
+        <div>
+            <MiniHeader></MiniHeader>
+            <article className={styles.article}>
+                <Head>
+                    <title>{title} | TCG Online</title>
+                    <meta name="description" content="need to add summary field to posts" />
+                    <meta property="og:title" content="{title} | TCG Online" />
+                    <meta
+                        property="og:description"
+                        content="need to add summary field to posts"
+                    />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <h1>{title}</h1>
+                <span className={styles['author-name']}>By {name}</span>
+                {categories && (
+                    <ul className={styles['category-list']}>
+                        {categories.map(category => <li key={category} className={styles['category-name']}>{category}</li>)}
+                    </ul>
+                )}
+                <BlockContent
+                    blocks={body}
+                    imageOptions={{ w: 320, h: 240, fit: 'max' }}
+                    {...client.config()}
+                />
+            </article>
+            <ArticleFooter></ArticleFooter>
+        </div>
+    );
+}
+
+export async function getStaticPaths() {
+    const paths = await client.fetch(
+        `*[_type == "post" && defined(slug.current)][].slug.current`
+    );
+  
+    return {
+        paths: paths.map((slug) => ({params: {slug}})),
+        fallback: true,
+    };
+}
+  
+export async function getStaticProps(context) {
+    // It's important to default the slug so that it doesn't return "undefined"
+    const { slug = "" } = context.params;
+    const post = await client.fetch(
+        groq`*[_type == "post" && slug.current == $slug][0]{
+                title,
+                "name": author->name,
+                "categories": categories[]->title,
+                body
+            }`
+        , { slug }
+    );
+    
+    return {
+        props: {
+            post
+        }
+    };
+}
+
+export default Post;
